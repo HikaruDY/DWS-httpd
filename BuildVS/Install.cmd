124925253c884fec31d83912ad4c6ee3d0411814
Hikaru Kurosaki <Hikaru@oss.xprj.org>
2023-04-14 01:51:32 +0900

    BuildVS: Optmize and Add support x64 platforms

--- BuildVS/Install.cmd
+++ BuildVS/Install.cmd
@@ -1,16 +1,34 @@
 @echo off
 
-rd /s /q "%~dp0\ServerRoot"
-md "%~dp0\ServerRoot"
-md "%~dp0\ServerRoot\bin"
-md "%~dp0\ServerRoot\conf"
-md "%~dp0\ServerRoot\modules"
-md "%~dp0\ServerRoot\logs"
-copy "%~dp0\Release\*.exe" "%~dp0\ServerRoot\bin"
-copy "%~dp0\Release\*.dll" "%~dp0\ServerRoot\bin"
-copy "%~dp0\Release\*.so" "%~dp0\ServerRoot\modules"
+pushd "%~dp0"
+
+IF EXIST "Release_x86" CALL :COLLECT x86
+IF EXIST "Release_x64" CALL :COLLECT x64
+IF EXIST "Release_ia64" CALL :COLLECT ia64
+IF EXIST "Release_a32" CALL :COLLECT a32
+IF EXIST "Release_a64" CALL :COLLECT a64
+IF EXIST "Release_a64ex" CALL :COLLECT a64ec
+
+
+GOTO :EOF
+
+:COLLECT
+rd /s /q "%~dp0\ServerRoot_%1"
+md "%~dp0\ServerRoot_%1"
+md "%~dp0\ServerRoot_%1\bin"
+md "%~dp0\ServerRoot_%1\conf"
+md "%~dp0\ServerRoot_%1\modules"
+md "%~dp0\ServerRoot_%1\logs"
+copy "%~dp0\Release_%1\*.exe" "%~dp0\ServerRoot_%1\bin"
+copy "%~dp0\Release_%1\*.dll" "%~dp0\ServerRoot_%1\bin"
+copy "%~dp0\Release_%1\*.so" "%~dp0\ServerRoot_%1\modules"
 
 pushd "%~dp0\..\srclib\openssl*\"
-copy "libeay32.dll" "%~dp0\ServerRoot\bin"
-copy "ssleay32.dll" "%~dp0\ServerRoot\bin"
+cd "BuildVS\Release_%1"
+copy "libcrypto-*.dll" "%~dp0\ServerRoot_%1\bin"
+copy "libssl-*.dll" "%~dp0\ServerRoot_%1\bin"
+popd
+GOTO :EOF
+
+:EOF
 popd
